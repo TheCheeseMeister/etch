1a981e08bb8344cadbb06392b1a63739df4f449e
TheCheeseMeister <ssomething511@gmail.com>
2024-07-11 18:56:37 -0400

    Added slider and button. Don't work yet. Also allowed for changing of grid size.
    Need to find some way to shrink it to fit in the container.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,39 @@
+function buildCanvas(size) {
+    let container = document.body.querySelector("#grid-container")
+    if (container != null) {
+        console.log(container);
+        container.remove();
+    }
+
+    let tempGrid = document.createElement("div");
+    tempGrid.setAttribute("id", "grid-container");
+
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+    
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.backgroundColor = "white";
+            square.style.height = "16px";
+            square.style.width = "16px";
+    
+            square.addEventListener("mouseover", function(e) {
+                if (e.buttons == 1) {
+                    square.style.backgroundColor = "black";
+                }
+            });
+    
+            row.appendChild(square);
+        }
+    
+        tempGrid.appendChild(row);
+    }
+
+    document.body.appendChild(tempGrid);
+}
+
 const gridContainer = document.querySelector("#grid-container");
 
 //gridContainer.style.backgroundColor = "red";
@@ -25,3 +61,5 @@ for (let i = 0; i < 16; i++) {
 
     gridContainer.appendChild(row);
 }
+
+buildCanvas(32);
