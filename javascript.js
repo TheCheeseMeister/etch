f6d46f4ff456a7d3a3a4078bd481e72824e66c96
TheCheeseMeister <ssomething511@gmail.com>
2024-07-11 19:12:30 -0400

    For now.

--- javascript.js
+++ javascript.js
@@ -34,32 +34,4 @@ function buildCanvas(size) {
     document.body.appendChild(tempGrid);
 }
 
-const gridContainer = document.querySelector("#grid-container");
-
-//gridContainer.style.backgroundColor = "red";
-gridContainer.style.width = "256px";
-
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.style.backgroundColor = "white";
-        square.style.height = "16px";
-        square.style.width = "16px";
-
-        square.addEventListener("mouseover", function(e) {
-            if (e.buttons == 1) {
-                square.style.backgroundColor = "black";
-            }
-        });
-
-        row.appendChild(square);
-    }
-
-    gridContainer.appendChild(row);
-}
-
 buildCanvas(32);
